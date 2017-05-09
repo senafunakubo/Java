4e534c480441cb4d72b2a09a5194154e1531f73d
Sena <inabacker24@gmail.com>
2017-05-09 13:31:26 -0700

    Add ex about employee

---
+++ Exercise_CICCC/ClassExercises/Exercise_May8/Lawyer.java
@@ -0,0 +1,18 @@
+package Exercise_May8;
+
+// CSE 142, Lab 9
+// A class to represent lawyers.
+
+public class Lawyer extends Employee {
+    public int getVacationDays() {
+        return super.getVacationDays() + 5;           // 3 weeks vacation
+    }
+
+    public String getVacationForm() {
+        return "pink";
+    }
+    
+    public void sue() {
+        System.out.println("I'll see you in court!");
+    }
+}
---
+++ Exercise_CICCC/ClassExercises/Exercise_May8/LegalSecretary.java
@@ -0,0 +1,14 @@
+package Exercise_May8;
+
+// CSE 142, Lab 9
+// A class to represent legal secretaries.
+
+public class LegalSecretary extends Secretary {
+    public double getSalary() {
+        return super.getSalary() + 5000.0;      // $45,000.00 / year
+    }
+    
+    public void fileLegalBriefs() {
+        System.out.println("I could file all day!");
+    }
+}
