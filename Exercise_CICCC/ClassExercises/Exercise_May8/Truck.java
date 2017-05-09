d0252a30be02d1e02d345504e36542457748e224
Sena <inabacker24@gmail.com>
2017-05-09 09:54:14 -0700

    Add classEx of May8

---
+++ Exercise_CICCC/ClassExercises/Exercise_May8/Truck.java
@@ -0,0 +1,15 @@
+package Exercise_May8;
+
+public class Truck extends Car {
+	public static void m1() {
+		System.out.println("truck 1");
+	}
+	 
+	public void m2() {
+		super.m1();
+	}
+	 
+	public String toString() {
+		return super.toString() + super.toString();
+	}
+}
