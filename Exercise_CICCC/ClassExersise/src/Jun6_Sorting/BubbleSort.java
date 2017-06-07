7bafd1fe3b11abc57feb3cbc5a4de65d11f39850
Sena <inabacker24@gmail.com>
2017-06-07 10:10:41 -0700

    Add some eps of sorting

---
+++ Exercise_CICCC/ClassExersise/src/Jun6_Sorting/BubbleSort.java
@@ -0,0 +1,62 @@
+package Jun6_Sorting;
+
+/**
+ * Created by senafunakubo on 2017-06-07.
+ */
+public class BubbleSort {
+
+        public static void BubbleSort(int[] num) { //上の要素と比較し、上のほうが大きければ互いに交換する
+            int j;
+            boolean flag = true;   // set flag to true to begin first pass
+            int temp;   //holding variable
+
+            while (flag) {
+                flag= false;
+
+                for( j=0;  j < num.length -1;  j++ )
+                {
+                    if ( num[j] < num[j+1] )   // change to > for ascending sort
+                    {
+                        temp = num[j];                //swap elements
+                        num[j] = num[j+1];
+                        num[j+1] = temp;
+                        flag = true;              //shows a swap occurred
+                    }
+                }
+
+            }
+            //Show the output
+        }
+
+        public static void SelectionSort(int[] num) {
+            for (int i = 0; i < num.length - 1; i++)
+            {
+                int index = i;
+                for (int j = i + 1; j < num.length; j++){
+                    if (num[j] < num[index]){
+                        index = j;//searching for lowest index
+                    }
+                }
+                int smallerNumber = num[index];
+                num[index] = num[i];
+                num[i] = smallerNumber;
+            }
+
+        }
+
+        public static void InsertionSort(int[] num) {
+            int j;                     // the number of items sorted so far
+            int key;                // the item to be inserted
+            int i;
+
+            for (j = 1; j < num.length; j++)    // Start with 1 (not 0)
+            {
+                key = num[j];
+                for(i = j-1; (i >= 0) && (num[i] < key); i--)   // Smaller values are moving up
+                {
+                    num[i+1] = num[i];
+                }
+                num[i+1] = key;    // Put the key in its proper location
+            }
+        }
+}
---
+++ Exercise_CICCC/ClassExersise/src/Jun6_Sorting/SetDemo.java
@@ -0,0 +1,76 @@
+package Jun6_Sorting;
+
+/**
+ * Created by senafunakubo on 2017-06-07.
+ */
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SetDemo {
+
+        public static void main(String[] args) {
+//              int[] num = {12, 45, 7, 42, 89, 99, 54, 23, 63, 52, 41, 75, 65, 54, 94, 68, 62, 43, 49, 89, 40, 60, 50, 70, 223, 336, 225};
+            int[] num = new int[1000];
+            for (int i = 0; i < 1000; i++) {
+                num[i] = (int) (Math.random()*1000);
+            }
+
+            Instant start = Instant.now();
+            BubbleSort.BubbleSort(num);
+            Instant end = Instant.now();
+            System.out.println("\ntime taken in worst case Bubble sort " + Duration.between(start, end));
+
+            start = Instant.now();
+            BubbleSort.SelectionSort(num);
+            end = Instant.now();
+            System.out.println("time taken in worst case Selection sort " + Duration.between(start, end));
+
+            start = Instant.now();
+            BubbleSort.InsertionSort(num);
+            end = Instant.now();
+            System.out.println("time taken in worst case Insertion sort " + Duration.between(start, end) + "\n");
+
+            Arrays.sort(num);
+
+
+            start = Instant.now();
+            BubbleSort.BubbleSort(num);
+            end = Instant.now();
+            System.out.println("\ntime taken in avg case Bubble sort " + Duration.between(start, end));
+
+            start = Instant.now();
+            BubbleSort.SelectionSort(num);
+            end = Instant.now();
+            System.out.println("time taken in best case Selection sort " + Duration.between(start, end));
+
+            start = Instant.now();
+            BubbleSort.InsertionSort(num);
+            end = Instant.now();
+            System.out.println("time taken in best case Insertion sort " + Duration.between(start, end) + "\n");
+
+
+            Collections.reverse(Arrays.asList(num));
+
+
+            start = Instant.now();
+            BubbleSort.BubbleSort(num);
+            end = Instant.now();
+            System.out.println("\ntime taken in best case Bubble sort " + Duration.between(start, end));
+
+            start = Instant.now();
+            BubbleSort.SelectionSort(num);
+            end = Instant.now();
+            System.out.println("time taken in avg case Selection sort " + Duration.between(start, end));
+
+            start = Instant.now();
+            BubbleSort.InsertionSort(num);
+            end = Instant.now();
+            System.out.println("time taken in avg case Insertion sort " + Duration.between(start, end) + "\n");
+
+            // do same for all sort algorithms. selection sort and insertion sort
+        }
+
+}
