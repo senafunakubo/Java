2a772797e34563782a512ca916b9f7e6657e1671
Sena <inabacker24@gmail.com>
2017-06-10 22:52:22 -0700

    String Helper
    doesn’t work

---
+++ Exercise_CICCC/ClassExersise/src/Jun9_Testing/StringHelper.java
@@ -0,0 +1,33 @@
+package Jun9_Testing;
+
+/**
+ * Created by senafunakubo on 2017-06-09.
+ */
+public class StringHelper {
+
+    public String truncateAInFirst2Positions(String str) {
+        if (str.length() <= 2)
+            return str.replaceAll("A", "");
+
+        String first2Chars = str.substring(0, 2);
+        String stringMinusFirst2Chars = str.substring(2);
+
+        return first2Chars.replaceAll("A", "") + stringMinusFirst2Chars;
+    }
+    //ABCD --> FALSE, ABAB--> TRUE, AB->TRUE A->FALSE
+
+    public boolean areFirstAndLastTwoCharactersTheSame(String str) {
+
+        if (str.length() <= 1)
+            return false;
+        if (str.length() == 2)
+            return true;
+
+        String first2Chars = str.substring(0, 2);
+
+        String last2Chars = str.substring(str.length() - 2);
+
+        return first2Chars.equals(last2Chars);
+    }
+
+}
